ac4989adf39237c3772bbe93942c248662faad5e
Fallen4Eyes <nova@nlpaige.me>
2020-11-16 09:44:32 -0600

    re-does chat look for a more minimal style

--- frontend/src/components/InfoBar.js
+++ frontend/src/components/InfoBar.js
@@ -11,11 +11,7 @@ import './InfoBar.css';
 const InfoBar = ({ room }) => (
   <div className="infoBar">
     <div className="leftInnerContainer">
-      <img className="onlineIcon" src={onlineIcon} alt="online icon" />
       <h3>{room}</h3>
-    </div>
-    <div className="rightInnerContainer">
-      <a href="/"><img src={closeIcon} alt="close icon" /></a>
     </div>
   </div>
 );
--- frontend/src/components/Message.js
+++ frontend/src/components/Message.js
@@ -7,27 +7,14 @@ import './Message.css';
 // would be cool to have custom emojis as well :)
 import ReactEmoji from 'react-emoji';
 
-const Message = ({message: { text, user }, name}) => {
+function Message({ message: { text, user }, name }) {
   let isSentByCurrentUser = user === name.trim().toLowerCase();
 
   return (
-    isSentByCurrentUser
-      ? (
-        <div className="messageContainer justifyEnd">
-          <p className="sentText pr-10">{name}</p>
-          <div className="messageBox backgroundBlue">
-            <p className="messageText colorWhite">{ReactEmoji.emojify(text)}</p>
-          </div>
-        </div>
-      )
-      : (
-        <div className="messageContainer justifyStart">
-          <div className="messageBox backgroundLight">
-            <p className="messageText colorDark">{ReactEmoji.emojify(text)}</p>
-          </div>
-          <p className="sentText pl-10 ">{user}</p>
-        </div>
-      )
+    <div className="messageContainer justifyEnd">
+      <p className={isSentByCurrentUser? "sender red" : "sender blue"}>{user}: </p>
+      <p className="message">{ReactEmoji.emojify(text)}</p>
+    </div>
   );
 }
 
